e0ca4351179c130b9ec8e5f90681d77aaa7d7d71
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-02 01:06:11 +0330

    shows Comment List test added

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
   const wrapper = shallow(<App />);
@@ -9,3 +10,8 @@ it('shows a comment box', () => {
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
+it('shows a Comment List', () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper.find(CommentList).length).toEqual(1);
+});
